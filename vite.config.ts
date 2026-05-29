1a6cbcc15af5cf0c2bc4d33dc2a8a4f34d02890d
axndyg <axndy.g@gmail.com>
2026-05-29 12:11:01 -0700

    reroute manifest to proper file stagings

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import webExtension from 'vite-plugin-web-extension'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), webExtension()],
 })
+
+
+
+
